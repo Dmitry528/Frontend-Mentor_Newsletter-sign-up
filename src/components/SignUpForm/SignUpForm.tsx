9a4c029bd472776ec03aca4d46456669dabdd01f
Dmitry528 <dmitrii.chekurkov@honeycombsoft.com>
2023-10-16 20:31:30 +0300

    finished email form

--- src/components/SignUpForm/SignUpForm.tsx
+++ src/components/SignUpForm/SignUpForm.tsx
@@ -1,7 +1,17 @@
 import signUpFormStyles from 'styles/modules/components/SignUpForm.module.css';
 
 export const SignUpForm = () => (
-  <div className={signUpFormStyles.signUpForm}>Signup form</div>
+  <form className={signUpFormStyles.signUpForm}>
+    <div className={signUpFormStyles.inputForm}>
+      <label className={signUpFormStyles.signUpForm__label}>Email address</label>
+      <input
+        className={signUpFormStyles.signUpForm__input}
+        type="text"
+        placeholder='email@company.com'
+      />
+    </div>
+    <button className={signUpFormStyles.signUpForm__button}>Subscribe to monthly newsletter</button>
+  </form>
 );
 
 export default SignUpForm;
