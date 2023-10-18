80da452c60458f4113354f4705654ab7c67477ae
Dmitry528 <dmitrii.chekurkov@honeycombsoft.com>
2023-10-18 21:50:02 +0300

    finished responsive design

--- src/components/SignUpImage/SignUpImage.tsx
+++ src/components/SignUpImage/SignUpImage.tsx
@@ -1,10 +1,12 @@
 import signUpImageStyles from 'styles/modules/components/SignUpImage.module.css';
 
 import desktopIllustration from 'assets/images/illustration-sign-up-desktop.svg';
+import mobileIllustration from 'assets/images/illustration-sign-up-mobile.svg';
 
 export const SignUpImage = () => (
   <div className={signUpImageStyles.signUpImage}>
     <picture>
+      <source srcSet={mobileIllustration} media='(max-width: 576px)' />
       <img src={desktopIllustration} alt='desktop illustration' />
     </picture>
   </div>
